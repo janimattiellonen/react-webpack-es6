40baa2e23c65ec88571a3e06221452902ef8f1d9
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2015-08-30 23:11:18 +0300

    - added arrow icons to previous/next buttons

--- src/components/Game/GamePlayView.js
+++ src/components/Game/GamePlayView.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Immutable from "immutable";
+import { Button, Glyphicon } from 'react-bootstrap';
 
 export default React.createClass({
 
 	render() {
 
 		return (
-			<h3>Peli</h3>			
+			<div>
+				<h3>Peli</h3>		
+
+
+				<Button><Glyphicon glyph='arrow-left' />Edellinen väylä</Button>	
+				<Button><Glyphicon glyph='arrow-right' className="pull-right"/>Seuraava väylä</Button>	
+			</div>
 		);
 	},
 });
